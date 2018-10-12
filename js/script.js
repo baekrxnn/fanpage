8429cd5e876491cf2c23083a8beb6a39c009202f
Karen He <hainechoko@gmail.com>
2018-10-12 01:24:01 +0000

    added gallery images and fixed fixed the sizing of the image in about. Started music player

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // JavaScript File
 /*global $*/
 
-$('body').click(function(){
+var songlist= [
+    https://www.youtube.com/embed/leu-cTvMWTA"
+    https://www.youtube.com/embed/sGRv8ZBLuW0"
+    https://www.youtube.com/embed/IdssuxDdqKk"
+    https://www.youtube.com/embed/tbe3pe2BtwA"
+    https://www.youtube.com/embed/KSH-FVVtTf0"
     
-});
-
-$("#about").click(function() {
     
-})
+    ]
